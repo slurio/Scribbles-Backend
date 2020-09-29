9b56c274653e7c93f7c2a2618adec2bf44a481ef
slurio <samantha.lurio@gmail.com>
2020-09-29 11:41:27 -0400

    post for scribble

--- app/controllers/background_canvases_controller.rb
+++ app/controllers/background_canvases_controller.rb
@@ -1,2 +1,25 @@
 class BackgroundCanvasesController < ApplicationController
+
+    def index
+
+        background_canvases = BackgroundCanvas.all
+
+        render json: background_canvases
+    end
+
+    def create
+        background = BackgroundCanvas.new(background_params)
+
+        if background.save
+          render json: background
+        else
+          render json: {error: background.errors.full_messages}, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def background_params
+        params.require(:background_canvas).permit(:background_style, :z_index, :scribble_id)
+    end
 end
--- app/serializers/background_canvas_serializer.rb
+++ app/serializers/background_canvas_serializer.rb
@@ -1,3 +1,5 @@
 class BackgroundCanvasSerializer < ActiveModel::Serializer
   attributes :id, :background_style, :z_index
+
+  belongs_to :scribble
 end
