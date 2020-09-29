eb8161f5fd8ab91a6f4297c045ce92357e34d7b5
slurio <samantha.lurio@gmail.com>
2020-09-29 15:47:55 -0400

    create of find user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,27 @@ class UsersController < ApplicationController
         render json: users
     end
 
+
     def show
+
         user = User.find(params[:id])
+ 
+        render json: user
+
+    end
+
+
+    def create
+
+        user = User.find_or_create_by(username: user_params[:username])
 
         render json: user
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
+
 end
