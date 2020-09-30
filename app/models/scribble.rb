f4976d25c955a27853373d237ac00d802fd5c4c1
anickel101 <anson.nickel@gmail.com>
2020-09-30 16:26:32 -0400

    added scribble destroy and dependent destroys

--- app/models/scribble.rb
+++ app/models/scribble.rb
@@ -1,5 +1,5 @@
 class Scribble < ApplicationRecord
   belongs_to :user
-  has_many :circle_canvases
-  has_one :background_canvas
+  has_many :circle_canvases, dependent: :destroy
+  has_one :background_canvas, dependent: :destroy
 end
